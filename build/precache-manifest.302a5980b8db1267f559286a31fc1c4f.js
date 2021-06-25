060ce84e0263afa2673e4bf54148a18b3f1fa03a
Andrii Shtadler <andrii_shtadler@epam.com>
2021-06-25 14:36:16 +0300

    as

---
+++ build/precache-manifest.302a5980b8db1267f559286a31fc1c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56988edc42e2be01ecc17291df88af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0bb9010c2f110803149",
+    "url": "/static/js/2.088d66a4.chunk.js"
+  },
+  {
+    "revision": "7f82e55884ef1cf5351544a9e10ebfb9",
+    "url": "/static/js/2.088d66a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7f22d3d8d63f6726dc",
+    "url": "/static/js/main.f6703ba7.chunk.js"
+  },
+  {
+    "revision": "6b0da550b69921a09089",
+    "url": "/static/js/runtime-main.21a5effb.js"
+  }
+]);
